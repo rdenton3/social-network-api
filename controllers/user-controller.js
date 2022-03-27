76ccadbacc749a727dc779905e2ca25679de220f
Rachel Denton <rdenton3@terpmail.umd.edu>
2022-03-27 09:13:34 -0400

    ability to perform CRUD operations on users

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,6 +5,47 @@ const userController = {
     createUser({body}, res) {
         User.create(body)
         .then(userData => res.json(userData))
+    },
+    // get all users
+    getAllUsers(req,res) {
+        User.find({})
+        .then(userData => res.json(userData))
+        .catch(err => {
+            console.log(err)
+            res.status(400).json(err)
+        })
+    },
+    // get user by id
+    getUserById({params},res) {
+        User.findOne({_id:params.id})
+        .then(userData => res.json(userData))
+        .catch(err => {
+            console.log(err)
+            res.status(400).json(err)
+        })
+    },
+    // update user by ID
+    updateUser({params, body}, res) {
+        User.findOneAndUpdate({_id:params.id},body, {new: true, runValidators:true})
+        .then(userData => {
+            if (!userData) {
+                res.status(404).json({message: "No user found with this ID!"})
+                return;
+            }
+            res.json(userData)
+        })
+        .catch(err => res.status(400).json(err))
+    },
+    deleteUser({params}, res) {
+        User.findOneAndDelete({_id:params.id})
+        .then(userData => {
+            if (!userData) {
+                res.status(404).json({message: "No user found with this ID!"})
+                return
+            }
+            res.json(userData)
+        })
+        .catch(err => res.status(400).json(err))
     }
 }
 module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,11 +1,23 @@
 const router = require('express').Router();
 const {
-    createUser
+    createUser,
+    getAllUsers,
+    getUserById,
+    deleteUser,
+    updateUser
   } = require('../../controllers/user-controller');
 
   // /api/users
 router
 .route('/')
 .post(createUser)
+.get(getAllUsers)
+
+  // /api/users/:id
+  router
+  .route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser)
 
 module.exports = router;
